4c1cf056807bf372d7b06f03f4430dec02037b70
Duong <duong.gary11@gmail.com>
2025-05-02 07:18:04 -0700

    Update rollthedice.c

--- rollthedice.c
+++ rollthedice.c
@@ -3,3 +3,13 @@
 #include <string.h>
 #include <math.h>
 #include <ctype.h>
+
+//Constants
+int d1 = [1,2,3,4,5,6]
+int d2 = [1,2,3,4,5,6]
+
+//main function
+int main() {
+  printf(%d, d1+d2);
+return 0;
+}
