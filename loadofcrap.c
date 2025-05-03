ce1403574677003400611a4f058e18e1b2b4aeeb
Duong <duong.gary11@gmail.com>
2025-05-03 04:06:42 -0700

    Update loadofcrap.c

--- loadofcrap.c
+++ loadofcrap.c
@@ -5,11 +5,43 @@
 #include <ctype.h>
 
 //Constants
+int prinamt = [$100]
 int d1 = [1,2,3,4,5,6]
 int d2 = [1,2,3,4,5,6]
+int mybet;
+int droll = [d1+d2]
+int point =
 
-//main function
+//Place Bets
 int main() {
-  printf(%d, d1+d2);
+  
+  printf(%s, "Principal Amount: %d", prinamt);
+  
+  printf("Enter Bet: \n");
+    scanf("%d", &mybet);
+  
+  printf("Principal Amount: %d", prinamt-mybet);
+
+//Pass or No Pass
+  printf(%s, "Pass or No Pass?");
+  /*user input for Pass or No Pass = y/n?*/
+  /*Main Function: Roll the Dice*/
+
+  printf("Press Enter to Roll the Dice!");
+  /*how do i want to input to roll the dice?*/
+  scanf()
+
+  printf(%d, droll);
+    If droll = 7 = no pass
+      else = pass
+
+pass = point
+if droll = point = win
+win = mybet*2+prinamt
+  7 = lose
+lose = mybet-prinamt
+
 return 0;
 }
+
+
